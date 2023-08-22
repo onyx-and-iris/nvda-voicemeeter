19f72dd255d63243c074b83c88eade8325b4e8d7
onyx-and-iris <code@onyxandiris.online>
2023-08-23 00:27:53 +0100

    fetches correct path if using compiled binary

--- src/nvda_voicemeeter/cdll.py
+++ src/nvda_voicemeeter/cdll.py
@@ -4,6 +4,8 @@ from pathlib import Path
 bits = 64 if ct.sizeof(ct.c_voidp) == 8 else 32
 
 controller_path = Path(__file__).parents[2].resolve() / "controllerClient"
+if not controller_path.exists():
+    controller_path = Path(__file__).parents[3].resolve() / "controllerClient"
 
 DLL_PATH = controller_path / f"x{64 if bits == 64 else 86}" / f"nvdaControllerClient{bits}.dll"
 
