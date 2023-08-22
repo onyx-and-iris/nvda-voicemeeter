5554286ee96a4ed819343addce62ed339df75aeb
onyx-and-iris <code@onyxandiris.online>
2023-08-22 18:10:10 +0100

    fix passing kind_id to voicemeeterlib

--- __main__.py
+++ __main__.py
@@ -8,6 +8,6 @@ logging.basicConfig(level=logging.DEBUG)
 
 kind_id = "potato"
 
-with voicemeeterlib.api("potato") as vm:
+with voicemeeterlib.api(kind_id) as vm:
     with nvda_voicemeeter.build(f"Voicemeeter {kind_id.capitalize()} NVDA", vm) as window:
         window.run()
