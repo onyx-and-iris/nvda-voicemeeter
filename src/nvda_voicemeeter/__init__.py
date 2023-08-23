4f48b0f01db6f9050a154bb507f4767d5693467d
onyx-and-iris <code@onyxandiris.online>
2023-08-23 16:13:33 +0100

    add a configurable delay before launching nvda

--- src/nvda_voicemeeter/__init__.py
+++ src/nvda_voicemeeter/__init__.py
@@ -1,12 +1,14 @@
 import subprocess as sp
+import time
 
 from .cdll import NVDA_PATH
 from .window import request_window_object as draw
 
 
-def launch():
+def launch(delay=1):
     if NVDA_PATH:
         sp.Popen([NVDA_PATH], shell=True)
+        time.sleep(delay)
 
 
 __ALL__ = ["launch", "draw"]
