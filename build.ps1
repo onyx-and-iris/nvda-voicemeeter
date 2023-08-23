3f696d86da71283d35b38a99b58047a4ce779392
onyx-and-iris <code@onyxandiris.online>
2023-08-23 16:39:50 +0100

    build and compress

--- build.ps1
+++ build.ps1
@@ -1,3 +1,20 @@
-@("basic", "banana", "potato") | ForEach-Object {
-    pdm run pyinstaller "${_}.spec" --noconfirm
+function Compress-Builds {
+    @("basic", "banana", "potato") | ForEach-Object {
+        $target = Join-Path -Path $PSScriptRoot -ChildPath "dist"
+        Compress-Archive -Path $(Join-Path -Path $target -ChildPath $_) -DestinationPath $(Join-Path -Path $target -ChildPath "${_}.zip")
+    } 
 }
+
+function Get-Builds {
+    @("basic", "banana", "potato") | ForEach-Object {
+        pdm run pyinstaller "${_}.spec" --noconfirm
+    }  
+}
+
+function main {
+    Get-Builds
+
+    Compress-Builds
+}
+
+if ($MyInvocation.InvocationName -ne '.') { main }
