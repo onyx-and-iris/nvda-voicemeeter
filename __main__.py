7d029216263aa49f14eac57afd59016c3de987b5
onyx-and-iris <code@onyxandiris.online>
2023-08-24 16:54:44 +0100

    sync the getters for now (todo: implement parameter callbacks)

--- __main__.py
+++ __main__.py
@@ -8,6 +8,6 @@ logging.basicConfig(level=logging.DEBUG)
 
 KIND_ID = "potato"
 
-with voicemeeterlib.api(KIND_ID) as vm:
+with voicemeeterlib.api(KIND_ID, sync=True) as vm:
     with nvda_voicemeeter.draw(f"Voicemeeter {KIND_ID.capitalize()} NVDA", vm) as window:
         window.run()
