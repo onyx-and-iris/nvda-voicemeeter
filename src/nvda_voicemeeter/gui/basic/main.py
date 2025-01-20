f48983d08a37473c2645b914c8b8a92cfca3d545
Onyx and Iris <code@onyxandiris.online>
2025-01-20 16:24:35 +0000

    add gui entry points

---
+++ src/nvda_voicemeeter/gui/basic/main.py
@@ -0,0 +1,11 @@
+import voicemeeterlib
+
+import nvda_voicemeeter
+
+
+def run():
+    KIND_ID = 'basic'
+
+    with voicemeeterlib.api(KIND_ID) as vm:
+        with nvda_voicemeeter.draw(KIND_ID, vm) as window:
+            window.run()
