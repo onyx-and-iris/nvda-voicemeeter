ff98aa5f899baa14c49dbf81098ed24651110256
onyx <code@onyxandiris.online>
2025-02-03 00:14:41 +0000

    run through ruff

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import voicemeeterlib
 
 import nvda_voicemeeter
 
-KIND_ID = "potato"
+KIND_ID = 'potato'
 
 with voicemeeterlib.api(KIND_ID) as vm:
     with nvda_voicemeeter.draw(KIND_ID, vm) as window:
